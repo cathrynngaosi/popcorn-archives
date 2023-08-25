78c7f3af25b6f9b4b948875b43895f2f130cdc86
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-08-25 17:42:20 +0800

    Setup tabs in my list page

--- src/pages/MyList.jsx
+++ src/pages/MyList.jsx
@@ -1,8 +1,31 @@
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import CreateNewList from "../components/CreateNewList";
+
 function MyList() {
   return (
-    <div>
-      <h1>My List</h1>
-    </div>
+    <>
+      <Tabs>
+        <TabList className="flex cursor-pointer items-center space-x-5 border-b-[1px] border-neutral-400 p-4 text-neutral-500 ">
+          <Tab>My Top Movies</Tab>
+          <Tab>My Top Series</Tab>
+          <Tab>Movies Bucketlist</Tab>
+          <Tab>Series Bucketlist</Tab>
+        </TabList>
+
+        <TabPanel className="text-white">
+          <h2>Top Movies</h2>
+        </TabPanel>
+        <TabPanel className="text-white">
+          <h2>Top Series</h2>
+        </TabPanel>
+        <TabPanel className="text-white">
+          <h2> Movies Bucketlist </h2>
+        </TabPanel>
+        <TabPanel className="text-white">
+          <h2> Series Bucketlist </h2>
+        </TabPanel>
+      </Tabs>
+    </>
   );
 }
 
