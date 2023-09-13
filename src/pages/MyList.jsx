a37201662c4756e82a498ef560f714b1aee05def
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-09-13 15:31:37 +0800

    Show tab according to URL

--- src/pages/MyList.jsx
+++ src/pages/MyList.jsx
@@ -1,17 +1,41 @@
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import { useList } from "../context/ListContext";
-import { Link, useNavigation } from "react-router-dom";
-import { useState } from "react";
+import { Link, useLocation, useNavigation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import LoadingScreen from "../components/LoadingScreen";
 import EmptyList from "../components/EmptyList";
 import MovieListContainer from "../components/MovieListContainer";
 
 function MyList() {
+  const location = useLocation();
   const { topMovies, topSeries, moviesBucketlist, seriesBucketlist } =
     useList();
   const [tabIndex, setTabIndex] = useState(0);
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
+
+  useEffect(() => {
+    async function checkURL() {
+      const urlList = location.pathname.split("/").pop();
+
+      switch (urlList) {
+        case "top-movies":
+          setTabIndex(0);
+          break;
+        case "top-series":
+          setTabIndex(1);
+          break;
+        case "movies-bucketlist":
+          setTabIndex(2);
+          break;
+        case "series-bucketlist":
+          setTabIndex(3);
+          break;
+      }
+    }
+
+    checkURL();
+  }, [location.pathname]);
 
   return (
     <>
