129fb9af98214709723aa874b95c7ad80b89ae03
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-09-13 15:39:03 +0800

    Update api url to https

--- src/services/apiMovies.js
+++ src/services/apiMovies.js
@@ -2,7 +2,7 @@ const OMDB_API_KEY = "3e7f2dde";
 
 export async function getMovies(query) {
   const res = await fetch(
-    `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${query}`,
+    `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${query}`,
   );
   const data = await res.json();
 
@@ -13,7 +13,7 @@ export async function getMovies(query) {
 
 export async function getMovieDetails(imdbID) {
   const res = await fetch(
-    `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${imdbID}`,
+    `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${imdbID}`,
   );
   const data = await res.json();
 
